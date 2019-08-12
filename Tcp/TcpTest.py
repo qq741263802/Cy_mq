135ccadc3ab24f58c6b9789e0c78ad51839cda01
lihuaming <13535485194@139.com>
2019-08-12 16:49:03 +0800

    dev1

--- Tcp/TcpTest.py
+++ Tcp/TcpTest.py
@@ -2,6 +2,6 @@
 import socket
 
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect(('94.191.97.65',30670))
-#s.send(b'ce61aebabd9dd82e|01|01|02|00|F0|55')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('94.191.97.65',30670))
+s.send(b'ce61aebabd9dd82e|01|01|02|00|F0|55')
